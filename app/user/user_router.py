3a015b8fab158670cd2cddec91ee33d687a75a42
ilop0 <ilop0624@gmail.com>
2026-01-14 14:26:50 +0900

    Added user_router.py and user_service.py, Made changes to index.html

--- app/user/user_router.py
+++ app/user/user_router.py
@@ -18,17 +18,68 @@ def login_user(user_login: UserLogin, service: UserService = Depends(get_user_se
 
 @user.post("/register", response_model=BaseResponse[User], status_code=status.HTTP_201_CREATED)
 def register_user(user: User, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
+    """
+    Register a new user.
+
+    This endpoint creates a new user with the given information.
+    If a user with the same email already exists, an error is raised.
+    
+    :param user: User information for registration.
+    :type user: User
+    :param service: User service dependency.
+    :type service: UserService
+    :return: A response containing the registered user information.
+    :rtype: Any
+    """
     ## TODO
-    return None
+    try:
+        new_user = service.register_user(user)
+        return BaseResponse(status="success", data=new_user, message="User registration success.")
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 @user.delete("/delete", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def delete_user(user_delete_request: UserDeleteRequest, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
+    """
+    Delete an existing user.
+
+    This endpoint deletes a user identified by the given email address.
+    If the user does not exist, an error is raised.
+    
+    :param user_delete_request: Request body containing the user's email.
+    :type user_delete_request: UserDeleteRequest
+    :param service: User service dependency.
+    :type service: UserService
+    :return: A response containing the deleted user information.
+    :rtype: Any
+    """
     ## TODO
-    return None
+    try:
+        user = service.delete_user(user_delete_request.email)
+        return BaseResponse(status="success", data=user, message="User Deletion Success.")
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
 
 @user.put("/update-password", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def update_user_password(user_update: UserUpdate, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
+    """
+    Update a user's password.
+
+    This endpoint updates the password of an existing user identified by email.
+    If the user does not exist, an error is raised.
+    
+    :param user_update: Request body containing the user's email and new password.
+    :type user_update: UserUpdate
+    :param service: User service dependency.
+    :type service: UserService
+    :return: A response containing the updated user information.
+    :rtype: Any
+    """
     ## TODO
-    return None
+    try:
+        user = service.update_user_pwd(user_update)
+        return BaseResponse(status="success", data=user, message="User password update success.")
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
--- app/user/user_service.py
+++ app/user/user_service.py
@@ -7,21 +7,47 @@ class UserService:
 
     def login(self, user_login: UserLogin) -> User:
         ## TODO
-        user = None
+        user = self.repo.get_user_by_email(user_login.email)
+        
+        if user == None:
+            raise ValueError("User not Found.")
+        
+        if user.password != user_login.password:
+            raise ValueError("Invalid ID/PW")
+        
         return user
         
     def register_user(self, new_user: User) -> User:
         ## TODO
-        new_user = None
-        return new_user
+        exist_user = self.repo.get_user_by_email(new_user.email)
+        
+        if exist_user != None:
+            raise ValueError("User already Exists.")
+
+        return self.repo.save_user(new_user)
 
     def delete_user(self, email: str) -> User:
-        ## TODO        
-        deleted_user = None
-        return deleted_user
+        ## TODO
+        user = self.repo.get_user_by_email(email)
+
+        if user == None:
+            raise ValueError("User not Found.")
+
+        return self.repo.delete_user(user)
 
     def update_user_pwd(self, user_update: UserUpdate) -> User:
         ## TODO
-        updated_user = None
+        user = self.repo.get_user_by_email(user_update.email)
+
+        if user == None:
+            raise ValueError("User not Found.")
+        
+        updated_user = User(
+            email=user.email,
+            password=user_update.new_password,
+            username=user.username
+        )
+
+        self.repo.save_user(updated_user)
         return updated_user
         
